5d4c8b14fa547b845c7b401bc5b57cde1c567e0d
Yasath Deshapriya <deshapriyayasath820@gmail.com>
2025-10-01 08:58:42 +0530

    done

--- Project_Euler/SpecialPythagoreanTriplet.py
+++ Project_Euler/SpecialPythagoreanTriplet.py
@@ -1,18 +1,14 @@
-def isPythTrip(a,b,c):
-    data = [a, b, c]
-    data.sort()
+def find_pythagorean_triplet(target):
+    for i in range(1,target):
+        for j in range(i+1,target-i):
+            k = 1000-i-j
+            if(i*i + j*j == k*k):
+                print(f'{i},{j},{k}')
+                print(i*j*k)
 
-    if(data[0]**2 + data[1]**2 == data[2]**2):
-        return True
-    else:
-        return False
-
-
-for i in range(1,1000):
-    for j in range(i,1000-i):
-        k = 1000-i-j
-        if(isPythTrip(i,j,k)):
-            return (i*j*k)
+find_pythagorean_triplet(1000)
+            
+            
 
             
 
